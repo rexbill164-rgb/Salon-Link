48270d73040526ad0323fe5a98de41dab5644655
rexbill164-rgb <rexbill164@gmail.com>
2026-05-03 11:50:10 +0000

    Restore generated script repair helper

---
+++ src/utils/generatedScriptRepairs.ts
@@ -0,0 +1,15 @@
+const SCRIPT_TEXT_REPAIRS: Array<[string, string]> = [
+  ["This provider hasn't submitted KYC documents yet.", 'This provider has not submitted KYC documents yet.'],
+  ["This provider hasn\\'t submitted KYC documents yet.", 'This provider has not submitted KYC documents yet.'],
+  ["Review takes 24-48 hrs. We'll email you.", 'Review takes 24-48 hrs. We will email you.'],
+  ["Review takes 24-48 hrs. We\\'ll email you.", 'Review takes 24-48 hrs. We will email you.'],
+  ["Review takes 24\u201348 hrs. We'll email you.", 'Review takes 24-48 hrs. We will email you.'],
+  ["Review takes 24\u201348 hrs. We\\'ll email you.", 'Review takes 24-48 hrs. We will email you.'],
+]
+
+export function repairInlineScriptText(html: string): string {
+  return SCRIPT_TEXT_REPAIRS.reduce(
+    (next, [broken, fixed]) => next.split(broken).join(fixed),
+    html,
+  )
+}
