712cb23da6550b38522b91d2d00b0d8f48aaa06c
rexbill164-rgb <rexbill164@gmail.com>
2026-05-03 22:33:01 +0000

    Wire messages and nearby service UI routes

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import reviewRoutes from './routes/reviews'
 import adminRoutes from './routes/admin'
 import uploadRoutes from './routes/uploads'
 import notificationRoutes from './routes/notifications'
+import messageRoutes from './routes/messages'
 
 // Page imports
 import { homePage } from './pages/home'
@@ -27,8 +28,10 @@ import { hairstyleHistoryPage } from './pages/hairstyleHistory'
 import { settingsPage } from './pages/settings'
 import { notificationsPage } from './pages/notifications'
 import { paymentPage, paymentSuccessPage } from './pages/paymentPage'
+import { messagesPage } from './pages/messages'
 import { repairInlineScriptText } from './utils/generatedScriptRepairs'
 import { withAdminKycDocumentViewer } from './utils/adminKycDocumentViewer'
+import { withDiscoveryNearbyUi, withProviderDashboardMessagesButton, withProviderProfileServiceUi } from './utils/chatMapServiceEnhancements'
 
 type Bindings = {
   DB: D1Database
@@ -57,18 +60,21 @@ app.route('/api/reviews', reviewRoutes)
 app.route('/api/admin', adminRoutes)
 app.route('/api/uploads', uploadRoutes)
 app.route('/api/notifications', notificationRoutes)
+app.route('/api/messages', messageRoutes)
 
 // Frontend pages
 app.get('/', (c) => c.html(homePage()))
 app.get('/login', (c) => c.html(loginPage()))
 app.get('/register', (c) => c.html(registerPage()))
 app.get('/dashboard', (c) => c.html(dashboardPage()))
-app.get('/provider/dashboard', (c) => c.html(repairInlineScriptText(providerDashboardPage())))
+app.get('/provider/dashboard', (c) => c.html(withProviderDashboardMessagesButton(repairInlineScriptText(providerDashboardPage()))))
 app.get('/provider/onboarding', (c) => c.html(onboardingPage()))
-app.get('/discover', (c) => c.html(discoveryPage()))
-app.get('/provider/:id', (c) => c.html(providerProfilePage(c.req.param('id'))))
+app.get('/discover', (c) => c.html(withDiscoveryNearbyUi(discoveryPage())))
+app.get('/provider/:id', (c) => c.html(withProviderProfileServiceUi(providerProfilePage(c.req.param('id')))))
 app.get('/book/:id', (c) => c.html(bookingPage(c.req.param('id'))))
 app.get('/admin', (c) => c.html(withAdminKycDocumentViewer(repairInlineScriptText(adminPanelPage()))))
+app.get('/messages', (c) => c.html(messagesPage()))
+app.get('/messages/:conversation_id', (c) => c.html(messagesPage(c.req.param('conversation_id'))))
 app.get('/hairstyle-history', (c) => c.html(hairstyleHistoryPage()))
 app.get('/settings', (c) => c.html(settingsPage()))
 app.get('/notifications', (c) => c.html(notificationsPage()))
